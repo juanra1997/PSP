6879a2aa89ad24eb2c99d96b26a27799c92780c6
juanra1997 <juanrainmortal22@yahoo.com>
2018-10-02 12:35:54 +0200

    +

--- PSP/src/Hilos/Ejemplo1.java
+++ PSP/src/Hilos/Ejemplo1.java
@@ -1,4 +1,7 @@
 package Hilos;
+
+import static java.lang.Thread.sleep;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,10 +12,62 @@ package Hilos;
  *
  * @author Juanra
  */
-public class Ejemplo1 {
+public class Ejemplo1 extends Thread {
     
-    public static void main(String args){
+    public Ejemplo1(String s){
+        
+        super(s);
+    }
+    
+    @Override
+    public void run(){
+        System.out.println("<<<<<<<<< Iniciando hilo: "+getName());
+        int cont=0;
+        while(cont<20){
+            dormir(1000);
+            System.out.println("\t\tCuenta atras > "+(20-cont));
+            cont++;
+        }
+    }
+    
+    public void dormir(long t){
+        try{
+            sleep(t);
+        } catch(InterruptedException e){
+            System.err.println("Error, mensaje: "+e.getMessage());
+        }
+    }
+    /*public static void main(String args){
         
         
+    }*/
+}
+
+class Principal {
+    
+    public static void main(String[] args){
+        
+        Ejemplo1 ca=new Ejemplo1("Hilo1");
+        long ti=System.currentTimeMillis();
+        System.out.println("##Hilos ejemplo 1\n<CASO 1 Heredando de Thread \n");
+        ca.start();
+        for(int i=0; i<10; i++){
+            System.out.println("Saludo: "+i);
+            dormir(500);
+        }
+        
+        while(ca.isAlive()){
+            
+        }
+        long te=(System.currentTimeMillis()-ti)/1000;
+        System.out.println("Final de programa, tiempo de ejecución: "+te);
+    }
+    
+    public static void dormir(long t){
+        try{
+            sleep(t);
+        } catch(InterruptedException e){
+            System.err.println("ERROR, mensaje: "+e.getMessage());
+        }
     }
 }
